d099be5a1779558cd039a2235fb3d58a0ceb7b64
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-17 20:55:02 +0900

    1001: a-b

---
+++ src/baekjoon/step1/AMinusB.java
@@ -0,0 +1,12 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+public class AMinusB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a - b);
+    }
+}
