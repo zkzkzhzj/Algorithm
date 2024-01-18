556a124280544ecd1702a5a2f5a3f1d284247f21
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-18 22:41:01 +0900

    10926: ??

---
+++ src/baekjoon/step1/Baek10926.java
@@ -0,0 +1,12 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+public class Baek10926 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println(userName+"??!");
+    }
+}
