1aebd7ad27b3c303af94b605b4db43c6f8750a92
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-18 21:48:48 +0900

    1008: 사칙연산

---
+++ src/baekjoon/step1/Baek1008.java
@@ -0,0 +1,17 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+public class Baek1008 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
