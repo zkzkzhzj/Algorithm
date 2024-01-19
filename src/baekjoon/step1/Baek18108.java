e183eec4109defaddc4949d2a4796cfdf297d938
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-19 23:00:23 +0900

    18108: 불기년도, 서기년도

---
+++ src/baekjoon/step1/Baek18108.java
@@ -0,0 +1,12 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+public class Baek18108 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        System.out.println(year-543);
+    }
+}
