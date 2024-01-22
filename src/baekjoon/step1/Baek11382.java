239e49a5a96f7c5adf3dbf1f24b3d2ffe33983b2
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-22 21:50:12 +0900

    11382: 10의 12승 = long

---
+++ src/baekjoon/step1/Baek11382.java
@@ -0,0 +1,14 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+public class Baek11382 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        System.out.println(a + b + c);
+    }
+}
