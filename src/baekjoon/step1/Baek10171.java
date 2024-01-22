7f5e0ebb918b535085f51f9be5cf91f64a40baff
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-23 08:37:19 +0900

    10171: 이스케이프 시퀀스

---
+++ src/baekjoon/step1/Baek10171.java
@@ -0,0 +1,10 @@
+package baekjoon.step1;
+
+public class Baek10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
