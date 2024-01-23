937b2a4b776cd5f0d738c91f45722513fafa7c0b
zkzkzhzj <zkzkzhzj@gmail.com>
2024-01-24 00:08:03 +0900

    10172: 개 그리기

---
+++ src/baekjoon/step1/Baek10172.java
@@ -0,0 +1,11 @@
+package baekjoon.step1;
+
+public class Baek10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
