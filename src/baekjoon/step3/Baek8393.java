2bebc74766ad0689369cd36bac0af79663b82bbe
zkzkzhzj <zkzkzhzj@gmail.com>
2024-02-04 20:47:14 +0900

    8393: n의 합 구하기

---
+++ src/baekjoon/step3/Baek8393.java
@@ -0,0 +1,18 @@
+package baekjoon.step3;
+
+import java.util.Scanner;
+
+public class Baek8393 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
